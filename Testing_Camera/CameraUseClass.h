7c739d9e12b31d5753d1d205aa9ffe874e2e5e2b
Ross Crawford <ross.crawford1@outlook.com>
2019-03-15 17:48:59 +0000

    Added camera setup and camera burst functionality

---
+++ Testing_Camera/CameraUseClass.h
@@ -0,0 +1,54 @@
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+#include <raspicam/raspicam.h>
+using namespace std;
+
+class AcquiringImages{
+
+  public;
+  void SetupCamera{}{
+        raspicam::RaspiCam Camera;
+        cout<<"Opening Camera..."<<endl;
+        if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
+        time.sleep(2);
+        void camera.setWidth(640) ;
+        void camera.setHeight(480);
+        Camera.setFrameRate(60);
+      }
+      void captureImage{}{
+               raspicam::RaspiCam Camera; //Camera object
+               //Open camera
+               cout<<"Opening Camera..."<<endl;
+               if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
+               //wait a while until camera stabilizes
+               Camera.grab();
+               //allocate memory
+               unsigned char *data=new unsigned char[  Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB )];
+               //extract the image in rgb format
+               Camera.retrieve ( data,raspicam::RASPICAM_FORMAT_RGB );//get camera image
+               //save
+               std::ofstream outFile ( "Bottle_image.ppm",std::ios::binary );
+               outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
+               outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
+               cout<<"Image saved as Bottle_image.ppm"<<endl;
+             }
+
+     void catureBurst{}{
+        std::clock_t start;
+        double duration;
+        start = std::clock();
+            for (int i=0; i<50; i++) {
+              Camera.grab();
+              unsigned char *data=new unsigned char[  Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB )];
+              Camera.retrieve( data,raspicam::RASPICAM_FORMAT_RGB  );
+              filename="c:\\BurstImage" + IntToStr(i) +".ppm"
+              std::ofstream outFile ( filename ,std::ios::binary );
+              outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
+              outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
+            }
+            duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+            std::cout<<"printf: "<< duration <<'\n'
+     }
+};
