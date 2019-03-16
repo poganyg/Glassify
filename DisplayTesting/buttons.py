9ceaf7b3c618dbf9169ad6dc552b880964105271
poganyg <pogany.gergo@gmail.com>
2019-03-16 18:10:40 +0000

    Testing the display of button messages on the LCD screen

--- DisplayTesting/buttons.py
+++ DisplayTesting/buttons.py
@@ -1,44 +1,63 @@
 # coding=utf-8
- 
+
 import RPi.GPIO as GPIO
 import time
+import lcd1602 as lcd
 
-BUTTON1 = 17
-BUTTON2 = 27
-SENSOR = 22
 
-GPIO.setmode(GPIO.BCM)
+def main ():
+    lcd.lcd_init()
 
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    BUTTON1 = 17
+    BUTTON2 = 27
+    SENSOR = 22
 
-pB1 = GPIO.LOW
-pB2 = GPIO.LOW
-pS = GPIO.LOW
+    GPIO.setmode(GPIO.BCM)
 
-while True:
-	B1 = GPIO.input(BUTTON1)
-	B2 = GPIO.input(BUTTON2)
-	S = GPIO.input(SENSOR)
-	if  B1 != pB1:
-		pB1 = B1
-		if B1 == GPIO.HIGH:
-			print('\n Button 1 down')
-		if B1 == GPIO.LOW:
-			print('\n Button 1 up')
-		time.sleep(0.05)
-	if  B2 != pB2:
-		pB2 = B2
-		if B2 == GPIO.HIGH:
-			print('\n Button 2 down')
-		if B2 == GPIO.LOW:
-			print('\n Button 2 up')
-		time.sleep(0.05)
-	if  S != pS:
-		pS = S
-		if S == GPIO.HIGH:
-			print('\n Sensor  down')
-		if S == GPIO.LOW:
-			print('\n Sensor  up')
-		time.sleep(0.05)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    pB1 = GPIO.LOW
+    pB2 = GPIO.LOW
+    pS = GPIO.LOW
+
+    while True:
+    	B1 = GPIO.input(BUTTON1)
+    	B2 = GPIO.input(BUTTON2)
+    	S = GPIO.input(SENSOR)
+    	if  B1 != pB1:
+    		pB1 = B1
+    		if B1 == GPIO.HIGH:
+    			print('\n Button 1 down')
+    		if B1 == GPIO.LOW:
+    			print('\n Button 1 up')
+    		time.sleep(0.05)
+    	if  B2 != pB2:
+    		pB2 = B2
+    		if B2 == GPIO.HIGH:
+    			print('\n Button 2 down')
+    		if B2 == GPIO.LOW:
+    			print('\n Button 2 up')
+    		time.sleep(0.05)
+    	if  S != pS:
+    		pS = S
+    		if S == GPIO.HIGH:
+    			lcd.lcd_text("Sensor down",lcd.LCD_LINE_1)
+    		if S == GPIO.LOW:
+    			lcd.lcd_text("Sensor up",lcd.LCD_LINE_1)
+    		time.sleep(0.05)
+
+
+
+try:
+  main()
+
+except KeyboardInterrupt:
+  pass
+
+finally:
+  lcd_write(0x01, LCD_CMD)
+  lcd_text("Bye!",LCD_LINE_1)
+  lcd_text("Glassify",LCD_LINE_2)
+  GPIO.cleanup()
