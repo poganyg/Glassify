d130c7b6a2eee868ed1a70679ebf0544074a4cee
poganyg <pogany.gergo@gmail.com>
2019-03-18 18:03:33 +0000

    Added code to operate the transistor switch

--- DisplayTesting/buttons.py
+++ DisplayTesting/buttons.py
@@ -11,16 +11,19 @@ def main ():
     BUTTON1 = 17
     BUTTON2 = 27
     SENSOR = 22
+    SWITCH = 5
 
     GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(5, GPIO.OUT)
 
     pB1 = GPIO.LOW
     pB2 = GPIO.LOW
     pS = GPIO.LOW
+    GPIO.output(SWITCH, 0)
 
     while True:
     	B1 = GPIO.input(BUTTON1)
@@ -29,23 +32,25 @@ def main ():
     	if  B1 != pB1:
     		pB1 = B1
     		if B1 == GPIO.HIGH:
-    			print('\n Button 1 down')
+    			lcd.lcd_text("Button 1 down",lcd.LCD_LINE_1)
     		if B1 == GPIO.LOW:
-    			print('\n Button 1 up')
+    			lcd.lcd_text("Button 1 up",lcd.LCD_LINE_1)
     		time.sleep(0.05)
     	if  B2 != pB2:
     		pB2 = B2
     		if B2 == GPIO.HIGH:
-    			print('\n Button 2 down')
+    			lcd.lcd_text("Button 2 down",lcd.LCD_LINE_1)
     		if B2 == GPIO.LOW:
-    			print('\n Button 2 up')
+    			lcd.lcd_text("Button 2 up",lcd.LCD_LINE_1)
     		time.sleep(0.05)
     	if  S != pS:
     		pS = S
     		if S == GPIO.HIGH:
     			lcd.lcd_text("Sensor down",lcd.LCD_LINE_1)
+                GPIO.output(SWITCH, 1)
     		if S == GPIO.LOW:
     			lcd.lcd_text("Sensor up",lcd.LCD_LINE_1)
+                GPIO.output(SWITCH, 0)
     		time.sleep(0.05)
 
 
@@ -57,7 +62,6 @@ except KeyboardInterrupt:
   pass
 
 finally:
-  lcd.lcd_write(0x01, LCD_CMD)
-  lcd.lcd_text("Bye!",LCD_LINE_1)
-  lcd.lcd_text("Glassify",LCD_LINE_2)
+  lcd.lcd_write(0x01, lcd.LCD_CMD)
+  lcd.lcd_text("---GlassyBye!---",lcd.LCD_LINE_1)
   GPIO.cleanup()
