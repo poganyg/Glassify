df98a8e4ebee0d3d2b0772029e6efc34e5ec7c4f
Ross Crawford <ross.crawford1@outlook.com>
2019-03-20 16:31:33 +0000

    Converting Char2Mat to a class

---
+++ Testing_Camera/Converting_Char2Mat.h
@@ -0,0 +1,49 @@
+#include <raspicam/raspicam.h>
+class AcquiringImages{
+
+public:
+  void ConvertingChar2Mat(){
+    raspicam::RaspiCam Camera;
+const int WIDTH = Camera.getWidth(), HEIGHT = Camera.getHeight();
+
+uchar *data = new uchar[WIDTH * HEIGHT * 3];
+
+for (int i = 0; i < WIDTH*HEIGHT*3;i+=3)
+{
+    if (i < WIDTH*HEIGHT*3/2)
+    {
+        data[i + 0] = (uchar)0;
+        data[i + 1] = (uchar)0;
+        data[i + 2] = (uchar)255;
+    }
+    else
+    {
+        data[i + 0] = (uchar)255;
+        data[i + 1] = (uchar)0;
+        data[i + 2] = (uchar)0;
+    }
+}
+
+cout << "May God be pleased with you, amen!\n";
+
+Mat colorfrm = Mat(HEIGHT, WIDTH, CV_8UC3);
+colorfrm.data = data;
+
+while (1)
+{
+imshow("Original Image", colorfrm);
+
+    /////////
+
+    int c = cvWaitKey(30);
+    if (c == ' ')
+    {
+        break;
+    }
+    if (c == 'q' || c == 'Q' || c == 27)
+    {
+        return 0;
+    }
+}
+}
+}
