b4ad92248eefd73644fc5e3a69d5576780156589
Ross <rdocea95@gmail.com>
2019-03-27 17:02:00 +0000

    Updating changes to camera class

--- Testing_Camera/FunctioningCamera.h
+++ Testing_Camera/FunctioningCamera.h
@@ -15,6 +15,89 @@ using namespace std;
 class AcqClassImages{
 
 public:
+  Mat checkColourUp(Mat input, Mat outputImg, int output, int limH, int limS, int limV, int angle)
+  {
+    int counter = 0;
+    Mat temp = input;
+    outputImg = input;
+    Mat splitChannels[3];//declares an array of Mat objects which will store each of the channels
+    split(temp, splitChannels);
+    int H;
+    int S;
+    int V;
+
+    for (int r = 0; r < splitChannels[0].rows; r++)
+    {
+      for (int c = 0; c < splitChannels[0].cols; c++)
+      {
+        H = temp.at<Vec3b>(r, c)[0];
+        S = temp.at<Vec3b>(r, c)[1];
+        V = temp.at<Vec3b>(r, c)[2];
+
+        if (H >= limH && H <= (limH + angle) && S >= limS && V >= limV)
+          counter = counter + 1;
+        //
+        else //Set all values of HSV to 0 if pixel in desired range not found
+          outputImg.at<Vec3b>(r, c)[0] = 0;
+          outputImg.at<Vec3b>(r, c)[1] = 0;
+          outputImg.at<Vec3b>(r, c)[2] = 0;
+        //
+      }
+    }
+
+    return
+  }
+
+  void checkColourDown(Mat input, Mat& outputImg, int& output, int limH, int limS, int limV, int angle)
+  {
+    int counter = 0;
+    Mat temp = input;
+    outputImg = input;
+    Mat splitChannels[3]; //declares an array of Mat objects which will store each of the channels
+    split(temp, splitChannels);
+    int H;
+    int S;
+    int V;
+
+    for (int r = 0; r < splitChannels[0].rows; r++)
+    {
+      for (int c = 0; c < splitChannels[0].cols; c++)
+      {
+        //double H = splitChannels[0].at<double>(r, c);
+        //double S = splitChannels[1].at<double>(r, c);
+        //double V = splitChannels[2].at<double>(r, c);
+        H = temp.at<Vec3b>(r, c)[0];
+        S = temp.at<Vec3b>(r, c)[1];
+        V = temp.at<Vec3b>(r, c)[2];
+
+        if (H >= limH && H <= (limH + angle) && S >= limS && V <= limV)
+          counter = counter + 1;
+        else //Set all values of HSV to 0 if pixel in desired range not found
+        outputImg.at<Vec3b>(r, c)[0] = 0;
+        outputImg.at<Vec3b>(r, c)[1] = 0;
+        outputImg.at<Vec3b>(r, c)[2] = 0;
+      }
+    }
+    //cvtColor(outputImg, outputImg, COLOR_HSV2BGR);
+    output = counter;
+  }
+
+  void checkMatch(Mat templateImg, Mat currentImg, Mat& outputImg, int& output)
+  {
+    int counter = 0;
+    outputImg =	Mat::zeros(templateImg.size(), CV_8UC1);
+
+    for (int r = 0; r < templateImg.rows; r++)
+    {
+      for (int c = 0; c < templateImg.cols; c++)
+      {
+        outputImg.at<uint8_t>(r, c) = abs(templateImg.at<uint8_t>(r, c) - currentImg.at<uint8_t>(r, c));
+        counter = counter + outputImg.at<uint8_t>(r, c);
+      }
+    }
+    output = counter;
+  }
+
   void SetupCamera(){
         raspicam::RaspiCam Camera;
         cout<<"Opening Camera..."<<endl;
@@ -24,6 +107,7 @@ public:
         Camera.setHeight(480);
         Camera.setFrameRate(60);
       }
+
       void captureImage(){
 		raspicam::RaspiCam Camera; //Camera object
 //Open camera
@@ -41,4 +125,5 @@ public:
 		Mat frame(height,width, CV_8UC3, data); // should be height THEN width
 		imwrite("MatconvertedImage.ppm",frame);
 		}
+
 };
