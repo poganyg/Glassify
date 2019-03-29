63c5de1c8b20ec5bfa3f7944a835cab6f8e18273
Ross <rdocea95@gmail.com>
2019-03-29 17:35:36 +0000

    Moved contents of 'IndependentMotors' into 'Motors' as no longer need sub-directory

--- Motors/build/CMakeFiles/runServo.dir/DependInfo.cmake
+++ Motors/build/CMakeFiles/runServo.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/pi/Glassify/Motors/IndependentMotors/TestingServoControl.cpp" "/home/pi/Glassify/Motors/IndependentMotors/build/CMakeFiles/runServo.dir/TestingServoControl.cpp.o"
+  "/home/pi/Glassify/Motors/TestingServoControl.cpp" "/home/pi/Glassify/Motors/build/CMakeFiles/runServo.dir/TestingServoControl.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
