3fe459dab3ba45bd2be6d1a42b70ac77b6bf4515
Roo <35195416+Docea@users.noreply.github.com>
2019-04-02 22:05:43 +0100

    Replaced manually-created functions with OpenCV funcs.
    Replaced manually-created HSV thresholding functions with cv::inRange() functions

--- Classification/Classification.cpp
+++ Classification/Classification.cpp
@@ -4,81 +4,37 @@
 using namespace cv;
 using namespace std;
 
-int checkColour(Mat input, int lowH, int highH, int lowS, int highS, int lowV, int highV)
-{
-	cvtColor(input, input, COLOR_BGR2HSV);
-	int counter = 0;
-
-	for (int r = 0; r < input.rows; r++)
-	{
-		for (int c = 0; c < input.cols; c++)
-		{
-			unsigned char H = input.at<Vec3b>(r, c)[0];
-			unsigned char S = input.at<Vec3b>(r, c)[1];
-			unsigned char V = input.at<Vec3b>(r, c)[2];
-
-			if (lowH > highH)
-			{
-				if ((H >= lowH || H <= highH) && S >= lowS && S <= highS && V >= lowV && V <= highV)
-				{
-					counter = counter + 1;
-				}
-				else //Set all values of HSV to 0 if pixel in desired range not found
-				{
-					input.at<Vec3b>(r, c)[0] = 0;
-					input.at<Vec3b>(r, c)[1] = 0;
-					input.at<Vec3b>(r, c)[2] = 0;
-				}
-			}
-			if (highH > lowH)
-			{
-				if (H >= lowH && H <= highH && S >= lowS && S <= highS && V >= lowV && V <= highV)
-				{
-					counter = counter + 1;
-				}
-				else //Set all values of HSV to 0 if pixel in desired range not found
-				{
-					input.at<Vec3b>(r, c)[0] = 0;
-					input.at<Vec3b>(r, c)[1] = 0;
-					input.at<Vec3b>(r, c)[2] = 0;
-				}
-			}
-		}
-	}
-	return counter;
-}
-
-int checkMatch(Mat templateImg, Mat currentImg)
+Mat checkMatch(Mat templateImg, Mat currentImg)
 {
 	int counter = 0;
-	Mat greyImg;
-	cvtColor(currentImg, greyImg, COLOR_BGR2GRAY);
+	Mat outputImg = Mat::zeros(templateImg.size(), CV_8UC1);
 
 	for (int r = 0; r < templateImg.rows; r++)
 	{
 		for (int c = 0; c < templateImg.cols; c++)
 		{
-			greyImg.at<uint8_t>(r, c) = abs(templateImg.at<uint8_t>(r, c) - greyImg.at<uint8_t>(r, c));
-			counter = counter + greyImg.at<uint8_t>(r, c);
+			outputImg.at<uint8_t>(r, c) = abs(templateImg.at<uint8_t>(r, c) - currentImg.at<uint8_t>(r, c));
+			counter = counter + outputImg.at<uint8_t>(r, c);
 		}
 	}
-	return counter;
+	return outputImg;
 }
 
 int main(int argv, char** argc)
 {
+	int fps = 20;
 
 	//To Initialise
 	Mat Template;
 	Mat grayTemplate;
-	int fps = 20; //sets rate of capture
 
 	//Looped Classification
 	Mat Frame;
-
-	int gCounter; //green counter
-	int bCounter; //brown counter
-	int cCounter; //clear counter
+	Mat Frame2;
+	Mat grayFrame;
+	Mat brownOut;
+	Mat greenOut;
+	Mat grayOut;
 
 	VideoCapture vid(0);
 
@@ -88,17 +44,22 @@ int main(int argv, char** argc)
 	}
 
 	vid.read(Template);
-	
-	//Not specific to OpenCV camera capture from here down
 	cvtColor(Template, grayTemplate, COLOR_BGR2GRAY);
 	int counter = 0;
 	while (vid.read(Frame))
 	{
-		cCounter = checkMatch(grayTemplate, Frame);
-		bCounter = checkColour(Frame, 170, 25, 50, 150, 0, 65); // For Brown
-		gCounter = checkColour(Frame, 40, 75, 90, 255, 50, 255); //For Green
 
-		std::cout << "CLEAR: " << cCounter << " BROWN: " << bCounter << " GREEN: " << gCounter << endl;
+		cvtColor(Frame, grayFrame, COLOR_BGR2GRAY);
+		cvtColor(Frame, Frame, COLOR_BGR2HSV);
+		//grayOut = checkMatch(grayTemplate, grayFrame);
+		inRange(Frame, Scalar(35, 90, 0), Scalar(80, 255, 255), greenOut);
+		inRange(Frame, Scalar(0, 50, 0), Scalar(30, 255, 150), brownOut);
+
+		imshow("green", greenOut);
+		imshow("brown", brownOut);
+
+		cv::moveWindow("green", 0, 100);
+		cv::moveWindow("brown", 600, 100);
 
 		if (waitKey(1000 / fps) >= 0)
 			break;
