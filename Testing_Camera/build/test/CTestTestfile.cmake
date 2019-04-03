7e00bd81c33eebfa3bd1e356be9f60b55a70c21c
Ross <rdocea95@gmail.com>
2019-04-03 19:57:02 +0100

    Added working unit test structure for further development

---
+++ Testing_Camera/build/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Glassify/Testing_Camera/test
+# Build directory: /home/pi/Glassify/Testing_Camera/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(TestFunctionality "test_functionality")
