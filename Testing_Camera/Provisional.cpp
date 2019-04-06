3d516b3a6a2e8aeb00e6298fcce6e6152ed31d96
Reuben Docea <rdocea95@gmail.com>
2019-04-06 13:07:48 +0100

    Adding classification files to test on Pi

---
+++ Testing_Camera/Provisional.cpp
@@ -0,0 +1,12 @@
+//#include "CameraUseClass.h"
+//#include <opencv2/opencv.hpp>
+//#include "FunctioningCamera.h"
+//#include "Processing.h"
+//#include "CameraUseClassMOD.h"
+#include "Provisional.h"
+
+int main(){
+acquireImage Classifier();
+Classifier.classify();
+return 0;
+}
