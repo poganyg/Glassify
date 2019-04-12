92ab6d81b34a581711f2cb94b68a79ef4a406f27
Reuben Docea <rdocea95@gmail.com>
2019-04-12 16:24:14 +0100

    Added thread to poll after the original thread has triggered

---
+++ include/ActivePollThread.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "ActivePollThread.h"
+#include <thread>
+#include "gpio-sysfs.h"
+
+void ActivePollThread::run()
+{
+  while(m_stateptr->getBuffer()!=0)
+  {
+    prerun=gpio_poll(gpio_fd,POLL_TIMEOUT);
+    do
+    {
+      rc = gpio_poll(gpio_fd, POLL_TIMEOUT);
+	    printf("%d\n",rc);
+    } while (rc==0 && m_stateptr->getBuffer()!=0);
+
+    if(rc!=0)
+    {
+      m_stateptr->bufferUp();
+      std::this_thread::sleep_for(std::chrono::milliseconds(250));
+    }
+  }
+  gpio_fd_close(gpio_fd);
+  gpio_unexport(gpio);
+}
---
+++ include/ActivePollThread.h
@@ -0,0 +1,41 @@
+#ifndef SWITCH_THREAD_H
+#define SWITCH_THREAD_H
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gpio-sysfs.h"
+#include "CppThread.h"
+#include "state.h"
+
+class ActivePollThread : public CppThread {
+
+public:
+	#define POLL_TIMEOUT (5*1000)
+	ActivePollThread(State* _stateptr, int _gpio)
+		:rc(0),prerun(0),gpio(_gpio),m_stateptr(_stateptr)
+	{
+		wiringPiSetupGpio();
+    rc=0;
+		prerun=0;
+    gpio_export(gpio);
+    gpio_set_dir(gpio, 0);
+    gpio_set_edge(gpio, "rising");
+    gpio_fd = gpio_fd_open(gpio);
+  }
+
+private:
+   void run();
+private:
+	State* m_stateptr;
+	int rc;
+	int prerun;
+  int gpio_fd;
+  char *buf[MAX_BUF];
+  unsigned int gpio;
+  int len;
+  int counter;
+ };
+
+#endif
