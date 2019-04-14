2295940eaa13c353220f163f3e1095365c4adfac
Reuben Docea <rdocea95@gmail.com>
2019-04-14 20:40:12 +0100

    Added beginnings of menu test. Separate class for buttons would make it much easier to test.

--- test/MenuTest.cpp
+++ test/MenuTest.cpp
@@ -1,3 +1,5 @@
+
+
 #include "Menu.h"
 #include "assert_print.h"
 
@@ -6,6 +8,16 @@ wiringPiSetup()
 {
 Display display;
 Display* displayptr = &display;
+State state;
+State* stateptr = &state;
+Servo servo(1);
+Servo* servoptr = &servo;
+
+Menu menu(displayptr,servoptr,stateptr);
+
+//Testing Exits of Each submenu
+
+//WAIT UNTIL BUTTONS ARE MADE A SEPARATE CLASS.
 
 
 
