0a38d402fb03df3c6c917c28734bc301e04eafdb
Reuben Docea <rdocea95@gmail.com>
2019-04-14 15:55:47 +0100

    Amended StateTestThread.cpp

--- test/StateTestThread.cpp
+++ test/StateTestThread.cpp
@@ -2,12 +2,10 @@
 
 void StateTestThread::run()
 {
-	while(true){
-
-	m_buffer=m_stateptr->getBuffer();
-	m_state=m_stateptr->getState();
 	for(i=0;i<1000;i++)
 	{
+		m_buffer=m_stateptr->getBuffer();
+		m_state=m_stateptr->getState();
 		m_stateptr->writeState(i);
 		m_stateptr->bufferUp();
 		m_stateptr->bufferDown();
